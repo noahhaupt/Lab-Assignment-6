a50571052fdd8d0ce82a79a6596f776952ed4946
ederyanael <ederyanael@gmail.com>
2025-10-30 09:10:58 -0400

    continued working during class time
    completed question 4 and started 5.

--- code.py
+++ code.py
@@ -8,13 +8,15 @@ This is a temporary script file.
 import pandas as pd
 import seaborn as sns
 
-world_data = pd.read_csv("Lab-Assignment-6/worlddata.csv")
+world_data = pd.read_csv("worlddata.csv")
 
 # Part 4
 # Question 1: 
 
-world_data["GNI per capita"] = world_data["GNI"] / world_data["Population"]
-gni_per_cap = round(world_data["GNI per capita"],2)
+world_data["GNI per capita"] = (world_data["GNI"] / world_data["Population"]).round(2)
+gni_per_cap = world_data["GNI per capita"]
+
+
 sns.relplot(data = world_data, x = "Life expectancy, female", y = gni_per_cap)
 sns.relplot(data = world_data, x = "Life expectancy, male", y = gni_per_cap)
 
@@ -25,3 +27,16 @@ sns.relplot(data = world_data, x = "Life expectancy, male", y = gni_per_cap, hue
 #Question 3:
 sns.relplot(data = world_data, x = "Life expectancy, female", y = gni_per_cap, hue = "Region", kind = "line", errorbar = "sd")
 sns.relplot(data = world_data, x = "Life expectancy, male", y = gni_per_cap, hue = "Region", kind = "line", errorbar = "sd")
+
+# Standard deviation is not an accurate representation of this dataset. You cannot see the standard deviation on the graph because there are the same amount of values as there are rows (217), meaning the standard deviation will be the same.
+
+
+"""""Anael Edery"""
+#Question 4:
+sns.lmplot(data = world_data, x = "Life expectancy, female", y ="GNI per capita")
+sns.lmplot(data = world_data, x = "Life expectancy, male", y = "GNI per capita")    
+    
+    
+# Question 5:
+sns.relplot(data = world_data, x ="Life expectancy, female", y = "Physicians", col = "Region")
+sns.relplot(data = world_data, x ="Life expectancy, male", y = "Physicians", col = "Region")
