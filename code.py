f29c1f82234694a782d7ac902c175290181bed84
Noah Haupt <noahhaupt1@icloud.com>
2025-10-31 09:47:52 -0400

    Worked on part 3

--- code.py
+++ code.py
@@ -5,12 +5,49 @@ Spyder Editor
 This is a temporary script file.
 """"""Noah Haupt"""
 
-import pandas as pd
-import seaborn as sns
+#Part 3:
+#Question 1
+    # What is the distribution of greenhouse gases across different regions?
+    # Is there a positive correlation between International tourism and a country's Gross National Income?
+    # Which region has the most amount of countries? Which region has the most amount of subregions? Intermediate Regions?
 
+#Question 2
+import pandas as pd
 world_data = pd.read_csv("worlddata.csv")
 
+#Question 3
+print(world_data.info())
+#There are 217 rows in this dataset. Population has 217 non-null values (not empty), meaning it has no empty values for this column because 217-217 = 0. Physicians has 207 non-null values, meaning it has 10 empty values because 217-207 = 10.
+
+#Question 4
+print(world_data.nunique())
+#The nunique() function provides the number of different, non-null values in each column. For example, Physicians has 207 because 10 of the values are empty, but Region has 5 because there are only 5 different values.
+
+#Question 5
+print(world_data.describe())
+#The describe() function provides important information for the numerical data only (that's why there's only 12 columns)  such as the count, the mean, the standard deviation, the min and the max, and the first three quartiles (Q1-Q3)
+
+#Question 6
+world_data["GNI per capita"] = (world_data["GNI"] / world_data["Population"]).round(2)
+gni_per_cap = world_data["GNI per capita"]
+
+#Question 7
+#a
+print(world_data["Region"].value_counts())
+# There are 54 countries in Africa, 50 countries in Asia, 47 countries in Europe, 46 countries in the Americas, and 19 countries in Oceania
+
+#b
+print(world_data["High Income Economy"].value_counts())
+# There are 67 high income economies.
+
+#Question 8
+print(pd.crosstab(world_data["Region"], world_data["High Income Economy"]))
+#There are 0 high income economies in Africa, 17 in Americas, 14 in Asia, 31 in Europe, and 4 in Oceania. This answer lines up with the previous question of how many High income countries there are (17+14+31+4 = 67)
+
+
+
 # Part 4
+import seaborn as sns
 # Question 1: 
 
 world_data["GNI per capita"] = (world_data["GNI"] / world_data["Population"]).round(2)
